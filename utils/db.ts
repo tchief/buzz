f72f6d3640abcda685d9397159209783a4da08ed
Sergii <cyber.tchief@gmail.com>
2023-12-16 14:59:02 +0200

    Fix

--- utils/db.ts
+++ utils/db.ts
@@ -3,7 +3,7 @@ import { createClient } from "supabase";
 const SUPABASE_URL = Deno?.env?.get("SUPABASE_URL")!;
 const SUPABASE_ANON_KEY = Deno?.env?.get("SUPABASE_ANON_KEY")!;
 
-export const supabase = createClient(
+export const supabase = SUPABASE_URL && SUPABASE_ANON_KEY ? createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY,
-);
+) : null;
